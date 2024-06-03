b496a3159b1fa46dffb237d48de48b8419559ec8
Ufuk ABRAVACI <ufkk1262@gmail.com>
2024-06-03 03:39:58 +0300

    Started project and created ProductController

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        
+        private readonly StoreContext _context;
+        
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task <ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            if(products == null || products.Count == 0)
+            {return NotFound();}
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")] 
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {return NotFound();}
+            return Ok(product);
+        }
+
+    }
+}
